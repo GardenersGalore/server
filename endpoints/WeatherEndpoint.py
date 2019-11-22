c9c8ce84808f4b9289d28351f329a7dec66bd54b
dolko <oliver.dolk@live.com.au>
2019-11-22 22:03:08 +1100

    cleaning stuff out a bit

--- endpoints/WeatherEndpoint.py
+++ endpoints/WeatherEndpoint.py
@@ -50,36 +50,5 @@ class WeatherEndpoint(Resource):
             res.append(single_res)
 
         final['data'] = res
-            # "moonrise_ts": 1574354124,
-            # "wind_cdir": "ESE",
-            # "rh": 83,
-            # "pres": 1007.21,
-            # "high_temp": 19.2,
-            # "sunset_ts": 1574412667,
-            # "ozone": 293.662,
-            # "moon_phase": 0.152223,
-            # "wind_gust_spd": 10.515,
-            # "snow_depth": 0,
-            # "clouds": 56,
-            # "ts": 1574341260,
-            # "sunrise_ts": 1574361726,
-            # "app_min_temp": 19.2,
-            # "wind_spd": 3.5945,
-            # "pop": 50,
-            # "wind_cdir_full": "east-southeast",
-            # "slp": 1013.75,
-            # "valid_date": "2019-11-22",
-            # "app_max_temp": 22.7,
-            # "vis": 20.9725,
-            # "dewpt": 17.1,
-            # "snow": 0,
-            # "uv": 0,
-            # "weather": {
-            #     "icon": "c03d",
-            #     "code": 803,
-            #     "description": "Broken clouds"
-            # },
-
-
 
         return final
